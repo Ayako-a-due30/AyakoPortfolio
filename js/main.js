c9120c17fdee37bf85d24b89467112a7538a1e2c
Ayako-a-due30 <sonnige.seite.str@gmail.com>
2023-09-07 11:23:49 +0900

    トップページアニメーション

--- js/main.js
+++ js/main.js
@@ -1,15 +1,65 @@
-// 変数宣言
+// ローディング画面--------------------
+
+const loading = document.querySelector('#loading');
+
+window.addEventListener('load',()=> {
+  loading.classList.add('loaded');
+});
+
+//PCページ:ファーストビューの下までスクロールしたらページトップでメニューを固定
+const viewHeight = window.innerHeight;
+
+window.addEventListener('scroll',function() {
+  const scrolled = window.scrollY;
+  if(window.scrollY>viewHeight){
+    navi.classList.add('fixed');
+  }else {
+    navi.classList.remove('fixed');
+  }
+});
+
+
+//worksアニメーション--------------------
+
+const works = document.querySelectorAll('.works-list');
+const showWorks =(entries)=> {
+  for (let i = 0; i<works.length; i++){
+    const keyframes = {
+      opacity:[0,1],
+      filter:['blur(20px)','blur(0)'],
+    };
+    const options = {
+      duration:900,
+      delay:i * 500,
+      fill:'forwards',
+    };
+    entries[i].target.animate(keyframes,options);
+  }
+};
+const workObserver = new IntersectionObserver(showWorks);
+works.forEach((work)=>workObserver.observe(work));
+
+// CONTACT文字数カウント-----------------------
+//textarea
+const message = document.querySelector('#message');
+//show-count
+const count = document.querySelector('#count');
+
+message.addEventListener('keyup',()=> {
+  count.textContent = message.value.length;
+});
+
+//レスポンシブ--------------------------
+//ハンバーガーメニュー--------------------
+
 // ハンバーガーメニューの要素
 const hamburgerMenu = document.querySelector(".hamburger-menu");
-
 // ハンバーガーメニューがクリックされた時に表示されるメニュー画面の要素
 const navi = document.getElementById("hamburger-navigation");
-
 // ハンバーガーメニュー内の各セクションの要素
 const hamburgerMenuSections = document.querySelectorAll(".hamburger-menu-section");
 // ハンバーガーメニュー内の各メニューの要素
 const headerMenuList = document.querySelectorAll(".header-menu-list");
-
 
 // マスク
 const mask = document.getElementById("mask");
@@ -19,19 +69,20 @@ hamburgerMenu.addEventListener("click", function () {
     hamburgerMenu.classList.toggle("active");
     navi.classList.toggle('active');
     mask.classList.toggle('active');
-    hamburgerMenuSections.toggle('active');
+    hamburgerMenuSections.forEach(section=>{
+      section.classList.toggle('active');
+    });
   });
 
-headerMenuList.addEventListener("click", function () {
+headerMenuList.forEach(menuItem=>{
+  menuItem.addEventListener("click",function(){
     hamburgerMenu.classList.toggle("active");
     navi.classList.toggle('active');
     mask.classList.toggle('active');
-    hamburgerMenuSections.toggle('active');
+    hamburgerMenuSections.forEach(section=>{
+      section.classList.toggle('active');
+    });
+
   });
+});;
 
-//   hamburgerMenu.classList.toggle("active");
-// navi.classList.toggle('active');
-
-//PCページ「ファーストビューの下までスクロールしたらページトップでメニューを固定」
-const scrolled = window.scrollY;
-const viewHeight = document.documentElement.clientHeight;
